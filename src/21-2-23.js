fabe53e1d7ff7f35c8171985b74ca527592ec8c6
阿正 <1062677159@qq.com>
2021-02-24 19:53:58 +0800

    feat: 23附加题和24基础题

--- src/21-2-23.js
+++ src/21-2-23.js
@@ -9,3 +9,20 @@
 //
 // 3.keep-alive 组件有什么作用？
 // 缓存被keep-alive包裹的组件状态,不需要重复渲染，大幅度提高性能
+
+// 4.说下vue生命周期钩子函数?
+// beforeCreate
+// created
+// beforeMount
+// mounted
+// beforeUpdate
+// updated
+// beforeDestroy
+// destroyed
+// errorCaptured
+//
+// 5.vue中computed和watch区别?
+// computed惰性监听,只有computed属性内使用到的响应式数据产生了变化computed的才会触发
+// computed一般适用于多数据影响一处的场景
+// watch主动监听,当watch监听的数据产生了变化,会直接触发
+// watch适用于一处数据影响多处的场景
---
+++ src/21-2-24.js
@@ -0,0 +1,28 @@
+// 1.React 的组件间通信都有哪些形式？
+// props
+// redux
+// 2.React 中如何实现路由懒加载？
+// 路由配置里 index = () => import(...)
+// 3.React 的生命周期函数都有哪些，分别有什么作用？
+// 用于初始化 state
+// constructor() {}
+// 用于替换 `componentWillReceiveProps` ，该函数会在初始化和 `update` 时被调用
+// 因为该函数是静态函数，所以取不到 `this`
+// 如果需要对比 `prevProps` 需要单独在 `state` 中维护
+// static getDerivedStateFromProps(nextProps, prevState) {}
+// 判断是否需要更新组件，多用于组件性能优化
+// shouldComponentUpdate(nextProps, nextState) {}
+// 组件挂载后调用
+// 可以在该函数中进行请求或者订阅
+// componentDidMount() {}
+// 用于获得最新的 DOM 数据
+// getSnapshotBeforeUpdate() {}
+// 组件即将销毁
+// 可以在此处移除订阅，定时器等等
+// componentWillUnmount() {}
+// 组件销毁后调用
+// componentDidUnMount() {}
+// 组件更新后调用
+// componentDidUpdate() {}
+// 渲染组件函数
+// render() {}
